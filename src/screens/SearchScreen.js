65cc765123d6b11db2d688dce13b9430ba41855f
Koushik Saha <koushik.saha666@gmail.com>
2021-03-13 20:29:18 +0600

    add search resules

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,18 +1,25 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
-import useResults from "../hooks/useResults";
+import useResults from '../hooks/useResults';
+import ResultsList from '../components/ResultsList';
 
 const SearchScreen = () => {
     const [term, setTerm] = useState('');
-
-    const [ searchApi, results, errorMessage] = useResults() ;
+    const [searchApi, results, errorMessage] = useResults();
 
     return (
         <View>
-            <SearchBar term={term} onTermChange={setTerm} onTermSubmit={searchApi} />
+            <SearchBar
+                term={term}
+                onTermChange={setTerm}
+                onTermSubmit={() => searchApi(term)}
+            />
             {errorMessage ? <Text>{errorMessage}</Text> : null}
             <Text>We have found {results.length} results</Text>
+            <ResultsList title="Cost Effective" />
+            <ResultsList title="Bit Pricier" />
+            <ResultsList title="Big Spender" />
         </View>
     );
 };
