a1f567673af278f17c363910514c2463e7b94961
Koushik Saha <koushik.saha666@gmail.com>
2021-03-13 18:59:16 +0600

    add yelp api

---
+++ src/api/yelp.js
@@ -0,0 +1,8 @@
+import axios from 'axios'
+
+export default axios.create({
+    baseUrl: 'https://api.yelp.com/v3/businesses',
+    headers: {
+        Authorization: 'Bearer VXwM1YpTcmpiqnKttexTh6lDZuBuoF_eszdgW56R277Y-9BR-qp1BtDjSNvXVlmcA6zrFsZ-b71NcDgFHC02EwQi62C2LJVy49IWMO452CXIx7YwuJn8A-QcEMa3X3Yx'
+    }
+});
